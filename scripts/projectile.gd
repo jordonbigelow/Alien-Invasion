245d02b738fd8e364bd5abee564d6596eb610461
jordonbigelow <the-music-dude@hotmail.com>
2026-04-23 22:01:05 -0600

    added projectile despawning upon them exiting the screenview

--- scripts/projectile.gd
+++ scripts/projectile.gd
@@ -5,3 +5,6 @@ var velocity = Vector2.UP * speed
 
 func _physics_process(delta: float) -> void:
 	position += velocity * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
