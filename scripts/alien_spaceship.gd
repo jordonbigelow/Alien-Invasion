3858b7e063af9ef2af3d1878c2b9da6ef4473b3f
jordonbigelow <the-music-dude@hotmail.com>
2026-04-24 20:26:28 -0600

    added logic that counts how many aliens there are, when the count reaches 0, the player wins

--- scripts/alien_spaceship.gd
+++ scripts/alien_spaceship.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
+signal died
 
 func _on_area_entered(area: Area2D) -> void:
-	print("dead")
+	died.emit()
 	area.queue_free()
 	queue_free()
--- scripts/combat_scene.gd
+++ scripts/combat_scene.gd
@@ -1,6 +1,19 @@
 extends Node2D
 
+var total_score: int = 0
+var remaining_aliens: int
+
 func _on_player_spaceship_shoot(projectile: Variant, location: Variant) -> void:
 	var projectile_instance = projectile.instantiate()
+	projectile_instance.add_to_group("projectiles")
 	add_child(projectile_instance)
 	projectile_instance.position = location
+
+func _on_alien_spaceship_died() -> void:
+	total_score += 1
+
+func _process(_delta: float) -> void:
+	remaining_aliens = get_tree().get_nodes_in_group("aliens").size()
+	if remaining_aliens <= 0:
+		print("total aliens killed: " + str(total_score))
+		get_tree().paused = true
