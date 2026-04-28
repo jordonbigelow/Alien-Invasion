b01be7dc8412e2abdfacda844fd819c3c6f46e69
jordonbigelow <the-music-dude@hotmail.com>
2026-04-28 16:25:48 -0600

    implemented grid like movement and steping down when the aliens hit a certain limit hard coded

--- scripts/alien_invaders.gd
+++ scripts/alien_invaders.gd
@@ -3,6 +3,8 @@ extends Node2D
 var total_score: int = 0
 var h_spacing: float = 45.0
 var v_spacing: float = 45.0
+var direction: int = 1
+var step_count: int = 0
 
 const ALIEN_SCENE: PackedScene = preload("res://scenes/alien_spaceship.tscn")
 
@@ -14,5 +16,19 @@ func _ready() -> void:
 				alien.connect("is_killed", _on_alien_spaceship_killed)
 				add_child(alien)
 
+func _process(delta: float) -> void:
+	_step()
+
 func _on_alien_spaceship_killed(points_for_kill: int) -> void:
 	total_score += points_for_kill 
+
+func _step() -> void:
+	position.x += 1.0 * direction
+	step_count += 1
+	if step_count >= 500:
+		_step_down()
+		step_count = 0
+		direction *= -1
+
+func _step_down() -> void:
+	position.y += 25
