7625999a737863b82412250266167177ecfb9944
jordonbigelow <the-music-dude@hotmail.com>
2026-04-28 16:40:18 -0600

    refactored the code for the grid based movement to use clampf

--- scripts/alien_invaders.gd
+++ scripts/alien_invaders.gd
@@ -5,6 +5,9 @@ var h_spacing: float = 45.0
 var v_spacing: float = 45.0
 var direction: int = 1
 var step_count: int = 0
+var h_step_amount: float = 1.0
+var left_limit: float = 30.0
+var right_limit: float = 670.0
 
 const ALIEN_SCENE: PackedScene = preload("res://scenes/alien_spaceship.tscn")
 
@@ -16,18 +19,17 @@ func _ready() -> void:
 				alien.connect("is_killed", _on_alien_spaceship_killed)
 				add_child(alien)
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	_step()
 
 func _on_alien_spaceship_killed(points_for_kill: int) -> void:
 	total_score += points_for_kill 
 
 func _step() -> void:
-	position.x += 1.0 * direction
-	step_count += 1
-	if step_count >= 500:
+	position.x = clampf(position.x, left_limit, right_limit)
+	position.x += h_step_amount * direction
+	if position.x >= right_limit or position.x <= left_limit:
 		_step_down()
-		step_count = 0
 		direction *= -1
 
 func _step_down() -> void:
