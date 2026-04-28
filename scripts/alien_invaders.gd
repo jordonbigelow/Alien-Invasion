effd00e8b8305bc5f86e39fcd21152df3bc32430
jordonbigelow <the-music-dude@hotmail.com>
2026-04-28 12:56:39 -0600

    fixed a bug where the aliens were not moving together using the Parent Node2D

--- scripts/alien_invaders.gd
+++ scripts/alien_invaders.gd
@@ -1,5 +1,18 @@
 extends Node2D
 
+var total_score: int = 0
+var h_spacing: float = 45.0
+var v_spacing: float = 45.0
+
+const ALIEN_SCENE: PackedScene = preload("res://scenes/alien_spaceship.tscn")
+
 func _ready() -> void:
-	position = Vector2(25.0, 25.0)
-	await get_tree().create_timer(1.0).timeout
+	for row in range(5):
+		for col in range(11):
+				var alien = ALIEN_SCENE.instantiate()
+				alien.position = Vector2(col * h_spacing, row * v_spacing)
+				alien.connect("is_killed", _on_alien_spaceship_killed)
+				add_child(alien)
+
+func _on_alien_spaceship_killed(points_for_kill: int) -> void:
+	total_score += points_for_kill 
--- scripts/combat_scene.gd
+++ scripts/combat_scene.gd
@@ -1,25 +1,12 @@
 extends Node2D
 
-var total_score: int = 0
 var remaining_aliens: int
 var projectiles_fired: int
-var h_spacing: float = 45.0
-var v_spacing: float = 45.0
-
-const ALIEN_SCENE: PackedScene = preload("res://scenes/alien_spaceship.tscn")
-
-func _ready() -> void:
-	for row in range(5):
-		for col in range(11):
-				var alien = ALIEN_SCENE.instantiate()
-				alien.position = Vector2(col * h_spacing + 25.0, row * v_spacing + 25.0)
-				alien.connect("is_killed", _on_alien_spaceship_killed)
-				add_child(alien)
 
 func _process(_delta: float) -> void:
 	remaining_aliens = get_tree().get_nodes_in_group("alien_invaders").size()
 	if remaining_aliens <= 0:
-		print("total score: " + str(total_score))
+		#print("total score: " + str(total_score))
 		print("total projectiles fired: "+ str(projectiles_fired))
 		get_tree().paused = true
 
@@ -28,6 +15,3 @@ func _on_player_spaceship_shoot(projectile: Variant, location: Variant) -> void:
 	add_child(projectile_instance)
 	projectile_instance.position = location
 	projectiles_fired += 1
-
-func _on_alien_spaceship_killed(points_for_kill: int) -> void:
-	total_score += points_for_kill 
