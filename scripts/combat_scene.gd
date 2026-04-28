846e3fac3d5c09ba3b059f70450d1906d9920f98
jordonbigelow <the-music-dude@hotmail.com>
2026-04-27 19:56:19 -0600

    idk

--- scripts/combat_scene.gd
+++ scripts/combat_scene.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 var total_score: int = 0
 var remaining_aliens: int
+var projectiles_fired: int
 
 func _on_player_spaceship_shoot(projectile: Variant, location: Variant) -> void:
 	var projectile_instance = projectile.instantiate()
 	projectile_instance.add_to_group("projectiles")
 	add_child(projectile_instance)
 	projectile_instance.position = location
+	projectiles_fired += 1
 
 func _on_alien_spaceship_killed() -> void:
 	total_score += 1
@@ -16,4 +18,5 @@ func _process(_delta: float) -> void:
 	remaining_aliens = get_tree().get_nodes_in_group("aliens").size()
 	if remaining_aliens <= 0:
 		print("total aliens killed: " + str(total_score))
+		print("total projectiles fired: "+ str(projectiles_fired))
 		get_tree().paused = true
